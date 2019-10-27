de24ebef988b9ebde1d9ad9af0a139acb85905a7
Benjamin <noreply@example.com>
2019-10-27 02:44:23 +0200

    Big improvements on glog parser, better robustness against malformed log files

--- src/parse/glog.rs
+++ src/parse/glog.rs
@@ -4,122 +4,145 @@ extern crate chrono;
 
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::Read;
 use chrono::prelude::*;
 
 // GLOG parser ----------------------------------------------------------------------
 
 pub fn to_log_entries(reader: impl std::io::Read) -> Vec::<model::LogEntry> {
-	let mut bufreader = BufReader::new(reader);
 	let mut log_entries = Vec::<model::LogEntry>::new();
-	let mut buf = Vec::<u8>::new();
-    while bufreader.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
-		match String::from_utf8_lossy(&buf) {
-			std::borrow::Cow::Borrowed(line_str) => {
-				//println!("{}", line_str);
-				let curr_entry = line_to_log_entry(&line_str);
-				log_entries.push(curr_entry);
-				buf.clear();
-			},
-			std::borrow::Cow::Owned(line_str) => {
-				line_to_log_entry(&line_str);
-				//TODO: Notify of invalid lines?
-				println!("MALFORMED UTF-8: {}", line_str);
-			},
-		}
-    }
-	log_entries
-}
-
-pub fn line_to_log_entry(line: &str) -> model::LogEntry {
-	let mut log_entry = model::LogEntry { ..Default::default() };
+	
 	let mut parser = GlogParserState::PreSection;
-	for idx in line.char_indices() {
+	let mut parser_str = Vec::with_capacity(512);
+	let mut log_entry = model::LogEntry { ..Default::default() };
+	
+	let mut bufreader = BufReader::new(reader);
+	let mut buffer = [0; 1];
+    while let Ok(bytes_read) = bufreader.read(&mut buffer){
+		if(bytes_read == 0) {
+			break;
+		}
+		let chr = buffer[0];
+		//println!("Char: {}", chr as i32);
+		
 		parser = match parser {
 			GlogParserState::PreSection => {
-				if idx.1 == GLOG_SECTION_BEGIN {
-					GlogParserState::SectionKind(idx.0 + GLOG_SECTION_BEGIN_SZ)
-				} else if idx.1 == GLOG_NEWLINE_R || idx.1 == GLOG_NEWLINE_N {
+				if chr == b'[' {
+					GlogParserState::SectionKind
+				} else if chr == b'\r' || chr == b'\n' {
 					GlogParserState::PreSection
 				} else {
-					GlogParserState::Invalid
+					println!("Invalid symbol encountered before log line: {}", chr);
+					GlogParserState::PreSection
 				}
 			},
-			GlogParserState::SectionKind(kind_offset) => {
-				if idx.1 == GLOG_INNER_DELIM {
-					let kind_str = &line[kind_offset..idx.0];
-					//println!("{}", &kind_str);
-					let kind = match kind_str {
-						"tq" => GlogSectionKind::TimestampMs,
-						"s" => GlogSectionKind::Severity,
-						"i" => GlogSectionKind::LogSource,
-						"m" => GlogSectionKind::Message,
-						_ => GlogSectionKind::Unknown, //TODO: Notify of invalid sections?
-					};
-					GlogParserState::SectionValue(kind, idx.0 + GLOG_INNER_DELIM_SZ)
-				} else {
-					GlogParserState::SectionKind(kind_offset)
-				}
-			},
-			GlogParserState::SectionValue(kind, value_offset) => {
-				if idx.1 == GLOG_SECTION_END {
-					GlogParserState::SectionValuePost(kind, value_offset)
-				} else {
-					GlogParserState::SectionValue(kind, value_offset)
-				}
-			},
-			GlogParserState::SectionValuePost(kind, value_offset) => {
-				if idx.1 == GLOG_SECTION_DELIM || idx.1 == GLOG_NEWLINE_R || idx.1 == GLOG_NEWLINE_N {
-					if idx.1 == GLOG_SECTION_DELIM && kind == GlogSectionKind::Message {
-						//Message is always last - ignore "]:"
-						GlogParserState::SectionValue(kind, value_offset)
-					} else {
-						//Add field to log entry
-						let value_str = &line[value_offset..idx.0-GLOG_SECTION_END_SZ];
-						//println!("Kind: {:?}, Value: {}", kind, value_str);
-						match kind {
-							GlogSectionKind::TimestampMs => {
-								if let Ok(ts_milli) = value_str.parse::<u64>() {
-									let ts_sec   : u64 = ts_milli / 1000;
-									let ts_nano  : u32 = ((ts_milli - ts_sec * 1000) * 1000_000) as u32;
-									if let Some(ndt) = NaiveDateTime::from_timestamp_opt(ts_sec as i64, ts_nano) {
-										log_entry.timestamp = DateTime::<Utc>::from_utc(ndt, Utc);
-									} else {
-										//TODO: Notify of invalid datetime?
-										println!("MALFORMED Log datetime: {}", value_str);
-									}
-								} else {
-									//TODO: Notify of invalid timestamp?
-									println!("MALFORMED Log timestamp: {}", value_str);
-								}
-							},
-							GlogSectionKind::Severity => {
-								if let Ok(glog_sev_u32) = value_str.parse::<u32>() {
-									if let Some(glog_sev) = GlogSeverity::from_u32(glog_sev_u32) {
-										log_entry.severity = normalize_glog_sev(glog_sev);
-									}
-								}
-							},
-							GlogSectionKind::Message => {
-								log_entry.message = value_str.to_string();
-							},
-							_ => (),
+			GlogParserState::SectionKind => {
+				if chr == b'|' {
+					let kind_str = std::str::from_utf8(&parser_str);
+					//println!("{}", &String::from_utf8_lossy(&parser_str));
+					
+					let kind = if let Ok(kind_str) = kind_str {
+						match kind_str.as_ref() {
+							"tq" => GlogSectionKind::TimestampMs,
+							"s" => GlogSectionKind::Severity,
+							"i" => GlogSectionKind::LogSource,
+							"m" => GlogSectionKind::Message,
+							_ => GlogSectionKind::Unknown, //TODO: Notify of invalid sections?
 						}
-						GlogParserState::PreSection
-					}
-				} else if idx.1 == GLOG_SECTION_END {
-					GlogParserState::SectionValuePost(kind, value_offset)
+					} else {
+						GlogSectionKind::Unknown //TODO: Notify of malformed UTF-8?
+					};
+					parser_str.clear();
+					GlogParserState::SectionValue(kind)
 				} else {
-					GlogParserState::SectionValue(kind, value_offset)
+					parser_str.push(chr);
+					GlogParserState::SectionKind
 				}
 			},
-			GlogParserState::Invalid => {
-				//TODO: Notify of invalid lines?
-				println!("MALFORMED Log line: {}", line);
-				break;
+			GlogParserState::SectionValue(kind) => {
+				parser_str.push(chr);
+				if chr == b']' {
+					GlogParserState::SectionValuePost1(kind)
+				} else {
+					GlogParserState::SectionValue(kind)
+				}
+			},
+			GlogParserState::SectionValuePost1(kind) => {
+				parser_str.push(chr);
+				if chr == b':' {
+					GlogParserState::SectionValuePost3(kind, 3, false)
+				} else if chr == b'\r' {
+					GlogParserState::SectionValuePost2(kind)
+				} else if chr == b'\n' {
+					GlogParserState::SectionValuePost3(kind, 3, true)
+				} else {
+					GlogParserState::SectionValue(kind)
+				}
+			},
+			GlogParserState::SectionValuePost2(kind) => {
+				parser_str.push(chr);
+				if chr == b'\n' {
+					GlogParserState::SectionValuePost3(kind, 4, true)
+				} else {
+					GlogParserState::SectionValue(kind)
+				}
+			},
+			GlogParserState::SectionValuePost3(kind, suffix_cutoff, entry_done) => {
+				parser_str.push(chr);
+				if chr == b'[' {
+					let value_str = String::from_utf8_lossy(&parser_str[0..parser_str.len()-suffix_cutoff]);
+					
+					match kind {
+						GlogSectionKind::TimestampMs => {
+							if let Ok(ts_milli) = value_str.parse::<u64>() {
+								let ts_sec   : u64 = ts_milli / 1000;
+								let ts_nano  : u32 = ((ts_milli - ts_sec * 1000) * 1000_000) as u32;
+								if let Some(ndt) = NaiveDateTime::from_timestamp_opt(ts_sec as i64, ts_nano) {
+									log_entry.timestamp = DateTime::<Utc>::from_utc(ndt, Utc);
+								} else {
+									//TODO: Notify of invalid datetime?
+									println!("MALFORMED Log datetime: {}", value_str);
+								}
+							} else {
+								//TODO: Notify of invalid timestamp?
+								println!("MALFORMED Log timestamp: {}", value_str);
+							}
+						},
+						GlogSectionKind::Severity => {
+							if let Ok(glog_sev_u32) = value_str.parse::<u32>() {
+								if let Some(glog_sev) = GlogSeverity::from_u32(glog_sev_u32) {
+									log_entry.severity = normalize_glog_sev(glog_sev);
+								}
+							}
+						},
+						GlogSectionKind::Message => {
+							log_entry.message = value_str.to_string();
+						},
+						_ => (),
+					}
+					if(entry_done) {
+						log_entries.push(log_entry);
+						log_entry = model::LogEntry { ..Default::default() };
+					}
+					parser_str.clear();
+					GlogParserState::SectionKind
+				} else {
+					GlogParserState::SectionValue(kind)
+				}
 			},
 		};
 	}
-	log_entry
+	
+	log_entries
+}
+
+enum GlogParserState {
+	PreSection,                               //expect '[', ignore '\r' or '\n'
+	SectionKind,                       //expect kind until '|' (kind may not contain '|')
+	SectionValue(GlogSectionKind),     //expect value until ']'
+	SectionValuePost1(GlogSectionKind), //expect ':' or '\r' or '\n', process line
+	SectionValuePost2(GlogSectionKind),
+	SectionValuePost3(GlogSectionKind, usize, bool),
 }
 
 fn normalize_glog_sev(glog_sev : GlogSeverity) -> model::LogLevel {
@@ -133,17 +156,6 @@ fn normalize_glog_sev(glog_sev : GlogSeverity) -> model::LogLevel {
 	}
 }
 
-const GLOG_SECTION_BEGIN : char = '[';
-const GLOG_SECTION_BEGIN_SZ : usize = 1; //GLOG_SECTION_BEGIN.len_utf8();
-const GLOG_INNER_DELIM : char = '|';
-const GLOG_INNER_DELIM_SZ : usize = 1; //GLOG_INNER_DELIM.len_utf8();
-const GLOG_SECTION_END : char = ']';
-const GLOG_NEWLINE_R : char = '\r';
-const GLOG_NEWLINE_N : char = '\n';
-const GLOG_SECTION_END_SZ : usize = 1; //GLOG_SECTION_END.len_utf8();
-const GLOG_SECTION_DELIM : char = ':';
-//const GLOG_SECTION_DELIM_SZ : usize = 1; //GLOG_SECTION_DELIM.len_utf8();
-
 enum GlogSeverity
 {
 	Critical = 0,
@@ -153,6 +165,7 @@ enum GlogSeverity
 	Info     = 4,
 	None     = 5,
 }
+
 impl GlogSeverity {
 	fn from_u32(value: u32) -> Option<GlogSeverity> {
 		match value {
@@ -175,13 +188,3 @@ enum GlogSectionKind {
 	Message,
 	Unknown,
 }
-
-enum GlogParserState {
-	PreSection,                               //expect '[', ignore '\r' or '\n'
-	SectionKind(usize),                       //expect kind until '|' (kind may not contain '|')
-	SectionValue(GlogSectionKind, usize),     //expect value until ']'
-	SectionValuePost(GlogSectionKind, usize), //expect ':' or '\r' or '\n', process line
-	Invalid,                                  //park in this state on parser error
-}
-
-// -------------------------------------------------------------------------------------------------------------
