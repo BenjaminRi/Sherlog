51727008abe5bace76ba25cf4cc2061426eb28f3
Benjamin <noreply@example.com>
2019-11-10 22:56:01 +0100

    Add the collapsing of multiple files in ringbuffer together into one log source. Add struct ConcatZipReader to achieve this goal.

--- src/parse/sfile.rs
+++ src/parse/sfile.rs
@@ -1,11 +1,15 @@
 extern crate zip;
 
+use std::io::Read;
 use super::super::model;
 use super::glog;
+use std::mem;
 
 pub fn from_file(path : &std::path::PathBuf) -> Result<model::LogSource, std::io::Error> {
 	let file = std::fs::File::open(&path).unwrap();
 	let mut archive = zip::ZipArchive::new(file).unwrap();
+	
+	let mut glog_files = Vec::new();
 	
 	let mut child_sources = Vec::new();
 	child_sources.reserve(archive.len());
@@ -20,13 +24,127 @@ pub fn from_file(path : &std::path::PathBuf) -> Result<model::LogSource, std::io
 			match extension.to_string_lossy().as_ref() {
 				// ../logfiles/example.glog
 				"glog" => {
-					let root = model::LogSource {name: stem.to_string(), children: {model::LogSourceContents::Entries(Vec::<model::LogEntry>::new()) } };
-					child_sources.push(glog::to_log_entries(file, root));
+					let mut s = stem.to_string();
+					strip_suffix(&mut s);
+					glog_files.push(ZipEntry { name: s, index: i });
 				},
 				_ => (),
 			}
 		}
 	}
 	
+	//Merge numbered files together (e.g. contr_ProcessManager and contr_ProcessManager_1)
+	//Note: The number was already stripped with strip_suffix.
+	glog_files.sort_unstable();
+	let mut deque = std::collections::VecDeque::new();
+	let mut name = "".to_string();
+	for file in glog_files {
+		println!("Glog file: {:?}", file);
+		if name != file.name {
+			if !deque.is_empty() {
+				let deque = mem::replace(&mut deque, std::collections::VecDeque::new());
+				let reader = ConcatZipReader::new(&mut archive, deque);
+				let root = model::LogSource {name: name, children: {model::LogSourceContents::Entries(Vec::<model::LogEntry>::new()) } };
+				child_sources.push(glog::to_log_entries(reader, root));
+			}
+			name = file.name;
+		}
+		deque.push_back(file.index);
+	}
+	if !deque.is_empty() {
+		let deque = mem::replace(&mut deque, std::collections::VecDeque::new());
+		let reader = ConcatZipReader::new(&mut archive, deque);
+		let root = model::LogSource {name: name, children: {model::LogSourceContents::Entries(Vec::<model::LogEntry>::new()) } };
+		child_sources.push(glog::to_log_entries(reader, root));
+	}
+	
 	Ok(model::LogSource {name: "example2_1".to_string(), children: {model::LogSourceContents::Sources(child_sources) } })
+}
+
+struct ConcatZipReader<'a, R: std::io::Read + std::io::Seek> {
+	archive : &'a mut zip::ZipArchive<R>,
+	file : Option<zip::read::ZipFile<'a>>,
+	indices : std::collections::VecDeque<usize>,
+}
+
+impl<'a, R: std::io::Read + std::io::Seek> ConcatZipReader<'a, R> {
+	fn new(
+		archive : &'a mut zip::ZipArchive<R>,
+		indices : std::collections::VecDeque<usize>) -> ConcatZipReader<'a, R> {
+		ConcatZipReader {archive: archive, file: None, indices : indices }
+	}
+}
+
+impl<'a, R: std::io::Read + std::io::Seek> std::io::Read for ConcatZipReader<'a, R> {
+     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+		loop {
+			if let Some(f) = &mut self.file {
+				let result = f.read(buf);
+				if let Ok(bytes) = result {
+					if bytes == 0 {
+						//file exhausted, retry with next file
+						self.file = None;
+						continue;
+					}
+				}
+				//ordinary read from the file
+				return result;
+			} else {
+				match self.indices.pop_front() {
+					Some(idx) => {
+						//need to open new file
+						let f = self.archive.by_index(idx)?;
+						unsafe {
+							//Due to the fact that file references archive and both are in the same struct,
+							//this cannot be done in safe Rust
+							self.file = Some(std::mem::transmute::<zip::read::ZipFile<'_>, zip::read::ZipFile<'a>>(f));
+						}
+						//retry with newly opened file
+						continue;
+					},
+					None => {
+						//file list exhausted, end of reader
+						return Ok(0);
+					},
+				}
+			}
+		}
+	}
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct ZipEntry {
+	name : String,
+	index : usize,
+}
+
+
+fn strip_suffix(s : &mut String) {
+	let storage_type = if s.ends_with("_v"){
+		//Virtual (in RAM)
+		"_v"
+	}else if s.ends_with("_p"){
+		//Persistent (on Flash storage)
+		"_p"
+	}else{
+		//Unknown storage type
+		""
+	};
+	
+	s.truncate(s.len() - storage_type.len());
+	
+	if let Some(offset) = s.rfind('_') {
+		if let Ok(_) = &s[offset+1..s.len()].parse::<u32>() {
+			//Numbered logfile. Discard ring buffer index.
+			s.truncate(offset);
+		}
+	}
+	
+	if s.ends_with("_ov"){
+		//Overview logs are merged with normal logs.
+		s.truncate(s.len() - "_ov".len());
+	}
+	
+	//Restore storage type suffix
+	s.push_str(storage_type);
 }
