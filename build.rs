3096b4895c62163c887644480128488ebbecd6d1
Benjamin <noreply@example.com>
2022-11-20 15:08:09 +0100

    Move from `winres` to `winresource` crate to fix bug https://github.com/mxre/winres/issues/40

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 #[cfg(windows)]
-extern crate winres;
+extern crate winresource;
 
 #[cfg(windows)]
 fn compile_resource() {
-	winres::WindowsResource::new()
+	winresource::WindowsResource::new()
 		.set_icon("icon.ico")
 		.compile()
 		.expect("Could not compile windows resource!");
